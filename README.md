f5dcf1b218de4ab90db0ec8b4aeca863fabab5d3
Brandon <brandonDustin@Brandons-MBP.home>
2019-12-14 15:02:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# aboutPage
+# aboutPage
+# aboutPage
+# aboutPage
+# GA_ABOUTPAGE
+# GA_ABOUTPAGE
---
+++ app.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("Thank You Your Contact Info Has Been Submitted. Brandon Will Contact You As Soon As Possible!!");
+  }
